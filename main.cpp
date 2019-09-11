920720629e2e408e2afc4bce8bee3247f5ce636f
Neil Weidinger <neilweidingern@gmail.com>
2019-09-11 23:12:39 +0200

    updated scene layout to three small spheres and one huge one as a 'floor'

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@
 int main(int argc, char* argv[]) {
     raytracer::Raytracer rt {};
 
-    rt.addSphere(geometry::Sphere{geometry::Vector{4, -4, -20}, 5});
-    rt.addSphere(geometry::Sphere{geometry::Vector{-5, 6, -15}, 5});
+    rt.addSphere(geometry::Sphere(geometry::Vector(-10, -2, -30), 5));
+    rt.addSphere(geometry::Sphere(geometry::Vector(0, -2, -21), 5));
+    rt.addSphere(geometry::Sphere(geometry::Vector(10, -2, -30), 5));
+    rt.addSphere(geometry::Sphere(geometry::Vector(0, -9997, 0), 9990));
 
     // idk how to pass in lights as temporaries, need to find a fix
     lighting::DirectionLight dl {geometry::Vector{-1, -1, -1}, 1.5};
